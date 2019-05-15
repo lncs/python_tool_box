b1e4caa9a333fff9ed4f72a6a953f8b3314a13c7
lncs <ln8429@163.com>
2019-05-15 14:21:15 +0800

    增加批量修改文件后缀名、批量替换文件内容、批量修改文件编码的实现

--- modify_suffix.py
+++ modify_suffix.py
@@ -1,19 +1,23 @@
-### 批量修改文件后缀
-
 import os
 
 
 def modify_suffix(old_suffix, new_suffix, path='./suffix_files/'):
+    """
+    :param old_suffix: 原后缀
+    :param new_suffix: 目标后缀
+    :param path: 保存需要修改后缀名文件的文件夹
+    :return:
+    """
     # 列出当前路径下的所有文件
     files = os.listdir(path)
-    print(files)
+    # print(files)
     for filename in files:
         # print(filename)
         portion = os.path.splitext(filename)
-        print("文件名:{},文件后缀名:{}".format(portion[0], portion[1]))
+        # print("文件名:{},文件后缀名:{}".format(portion[0], portion[1]))
         if old_suffix == portion[1]:
-            newname = portion[0] + new_suffix
-            os.rename(path+filename, path+newname)
+            new_name = portion[0] + new_suffix
+            os.rename(path + filename, path + new_name)
             pass
 
 
@@ -21,5 +25,6 @@ if __name__ == '__main__':
     # 修改原子函数
     # modify_suffix('.uftfunction', '.uftatomfunction', )
     # 修改原子服务
+    # modify_suffix('.uftfunction', '.uftatomservice', )
     modify_suffix('.uftatomservice', '.uftfunction', )
     pass
