c6ae32d2b45be4a76d44d3ae61648eb087a3fed4
lncs <ln8429@163.com>
2019-05-15 15:17:10 +0800

    新增sftp连接相关功能

--- sftp_test.py
+++ sftp_test.py
@@ -18,7 +18,8 @@ def ftpconnect(host, port, username, password):
     sftp = paramiko.SFTPClient.from_transport(sf)
     return sftp
 
-#从ftp下载文件
+
+# 从ftp下载文件
 def downloadfile(sftp, remotepath, localpath):
     try:
         if os.path.isdir(localpath):  # 判断本地参数是目录还是文件
@@ -30,7 +31,8 @@ def downloadfile(sftp, remotepath, localpath):
         print('download exception:', e)
     sftp.close()
 
-#从本地上传文件到ftp
+
+# 从本地上传文件到ftp
 def uploadfile(sftp, remotepath, localpath):
     try:
         if os.path.isdir(localpath):  # 判断本地参数是目录还是文件
@@ -42,11 +44,12 @@ def uploadfile(sftp, remotepath, localpath):
         print('download exception:', e)
     sftp.close()
 
+
 if __name__ == "__main__":
     # ftp地址
     ftp = ftpconnect("192.168.102.184", 1111, "fmtest", "hsfundYrs04f")
     print("登录成功")
-    downloadfile(ftp, "/资料/UFT代码同步工具/", "E:/Git/python/tool_box/downloads/")
+    downloadfile(ftp, "/资料/UFT代码同步工具/", "./downloads/")
     # #调用本地播放器播放下载的视频
     # os.system('start "C:\Program Files\Windows Media Player\wmplayer.exe" "C:/Users/Administrator/Desktop/test.mp4"')
     # uploadfile(ftp, "C:/Users/Administrator/Desktop/test.mp4", "test.mp4")
